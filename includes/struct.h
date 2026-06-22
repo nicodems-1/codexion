8e8633d94d71afe7484778f28080c307eb932637
Nicodeme Verdier <niverdie@z1r12p3.42lyon.fr>
2026-06-22 22:51:06 +0200

    prototyping parser

---
+++ includes/struct.h
@@ -0,0 +1,9 @@
+typedef struct MyStructure {
+    int numbers_of_coders;
+    int time_to_compile;
+    int time_to_debug;
+    int time_to_refactor;
+    int numbers_of_compiles_required;
+    int dongle_cooldown;
+    int scheduler;
+} Param;
---
+++ src/parsing.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include "../includes/struct.h"
+
+void    parsing(int ac, char **av)
+{
+    Param p;
+
+}
+
+int main(int ac, char **av)
+{
+    parsing(ac, av)
+    Param el_data;
+    el_data.numbers_of_coders = 3;
+    printf("%d\n", el_data.numbers_of_coders);
+}
